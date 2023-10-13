bc666523bedf19d695806a1c307e30c2b90d008e
Julien Acker <work@julienacker.dev>
2023-10-13 16:55:29 +0100

    Improved Task2/3. Removed regression of Task 2 recomputing hashes

--- task1.py
+++ task1.py
@@ -1,42 +1,53 @@
-from numpy import base_repr
 from multiprocessing import Manager, Process, cpu_count, Event, Barrier
 import hashlib
 import time
 
 def crack(i: int, num: int, hashes: list, results: dict, change: Event, barrier: Barrier):
-    local_hashes = set(hashes)
-    while not change.is_set():
-        plaintext = base_repr(i, 36).lower()
-        hashed = hashlib.sha512(plaintext.encode()).hexdigest()
-        if hashed in local_hashes:
-            hashes.remove(hashed)
-            results[hashed] = plaintext
-            change.set()
-        i += num   
-    if not len(hashes) == 0:
-        barrier.wait()
-        change.clear()
-        crack(i, num, hashes, results, change, barrier)
+    digits = '0123456789abcdefghijklmnopqrstuvwxyz'
+    while True:
+        local_hashes = set(hashes)
+        while not change.is_set():
+            # Adapted from numpy's base_repr()
+            n = i
+            res = []
+            while n:
+                res.append(digits[n % len(digits)])
+                n //= len(digits)
+            plaintext = ''.join(reversed(res or '0'))
+
+            hashed = hashlib.sha512(plaintext.encode()).hexdigest()
+            
+            if hashed in local_hashes:
+                hashes.remove(hashed)
+                results[hashed] = plaintext
+                change.set()
+            i += num   
+        
+        if not len(hashes) == 0:
+            barrier.wait()
+            change.clear()
+        else:
+            return
 
 def brute(list_of_hashes) -> list:
     try:
-        processe_count = cpu_count()
+        process_count = cpu_count()
     except NotImplementedError:
-        processe_count = 4
+        process_count = 4
 
     with Manager() as manager:
         hashes = manager.list(list_of_hashes)
         results = manager.dict()
-        barrier = Barrier(processe_count)
+        barrier = Barrier(process_count)
         processes = []
         change = Event()
-        for processe_num in range(processe_count):
-            processe = Process(target=crack, args=(processe_num, processe_count, hashes, results, change, barrier))
-            processes.append(processe)
-            processe.start()
-        for processe in processes:
-            processe.join()    
-        return [results[item] for item in list_of_hashes] # ensures the order of results matches the input order
+        for process_num in range(process_count):
+            process = Process(target=crack, args=(process_num, process_count, hashes, results, change, barrier))
+            processes.append(process)
+            process.start()
+        for process in processes:
+            process.join()    
+        return [results[item] for item in list_of_hashes] # ensures the order of hashes matches the input order
 
 if __name__ == '__main__':
     hashes = ['f14aae6a0e050b74e4b7b9a5b2ef1a60ceccbbca39b132ae3e8bf88d3a946c6d8687f3266fd2b626419d8b67dcf1d8d7c0fe72d4919d9bd05efbd37070cfb41a', 'e85e639da67767984cebd6347092df661ed79e1ad21e402f8e7de01fdedb5b0f165cbb30a20948f1ba3f94fe33de5d5377e7f6c7bb47d017e6dab6a217d6cc24','4e2589ee5a155a86ac912a5d34755f0e3a7d1f595914373da638c20fecd7256ea1647069a2bb48ac421111a875d7f4294c7236292590302497f84f19e7227d80', 'afd66cdf7114eae7bd91da3ae49b73b866299ae545a44677d72e09692cdee3b79a022d8dcec99948359e5f8b01b161cd6cfc7bd966c5becf1dff6abd21634f4b']
